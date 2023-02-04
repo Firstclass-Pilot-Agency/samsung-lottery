e5d656da4816c5c09f9043a2421fc3d5d1780e64
HypeDeveloper <de.hypedeveloper@gmail.com>
2023-02-04 12:07:36 +0100

    up commite

--- frontend/src/lotterydb/admin.js
+++ frontend/src/lotterydb/admin.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 // handle Auth for admin
-const url = 'http://localhost:8000/api'
+const url = '/api'
 
 
 export async function authAdmin(token) {
--- frontend/src/lotterydb/usersService.js
+++ frontend/src/lotterydb/usersService.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const url = 'http://localhost:8000/api'
+const url = '/api'
 
 export const getUsers = async () => {
     const users = await axios.get(url+"/users")
